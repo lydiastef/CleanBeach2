f87d609e0dee062a0027d2a2fce338d2e13bb31e
Erla-B <111748576+Erla-B@users.noreply.github.com>
2023-03-22 14:49:10 +0000

    Cards almost ready

--- frontend/src/components/Cards/Cards.jsx
+++ frontend/src/components/Cards/Cards.jsx
@@ -2,52 +2,72 @@ import "./Cards.scss";
 import { NavLink } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import {AiFillCaretDown} from 'react-icons/ai';
+import { AiFillCaretDown } from "react-icons/ai";
 import beachService from "../../services/beachService";
+import ReactDOM from "react-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleArrowRight } from "@fortawesome/free-solid-svg-icons";
+
+const arrow = <FontAwesomeIcon icon={faCircleArrowRight} fade />;
 
 const Cards = () => {
   let [data, setData] = useState([]);
-  let [filter, setFilter] = useState(false)
-  const[isActive, setIsActive] = useState(false);
-  const [selectedValue, setSelectedValue] = useState('');
+  let [filter, setFilter] = useState(false);
+  const [isActive, setIsActive] = useState(false);
+  const [selectedValue, setSelectedValue] = useState("");
 
   function handleClick(event) {
-    setSelectedValue(event.target.getAttribute('name'));
-    setFilter(true)
-    
+    setSelectedValue(event.target.getAttribute("name"));
+    setFilter(true);
   }
-  if(filter){
-      data = data.filter(element => element.status === selectedValue)
+  if (filter) {
+    data = data.filter((element) => element.status === selectedValue);
   }
 
   useEffect(() => {
     const getData = async () => {
-      beachService.getAllBeaches().then(res => {
-        const result = res
+      beachService.getAllBeaches().then((res) => {
+        const result = res;
         setData(result.data);
-        console.log(result.data)
-      })
-    }
+        console.log(result.data);
+      });
+    };
     getData();
   }, [setData]);
 
   return (
     <>
-        <div className='dropdown'>
-                <div className='dropdown-btn' onClick={(e) => setIsActive(!isActive)}>
-                    Sort by <AiFillCaretDown style={{ color: 'white' }}/>
-                </div>
-                {isActive && (
-                <div className='dropdown-content'>
-                    <div className='dropdown-item' name="EXCELLENT" onClick={handleClick}>Excellent</div>
-                    <div className='dropdown-item' name="GOOD" onClick={handleClick}>Good</div>
-                    <div className='dropdown-item' name="SUFFICIENT" onClick={handleClick}>Sufficient</div>
-                    <div className='dropdown-item' name="POOR" onClick={handleClick}>Poor</div>
-                </div>
-                )}
+      <div className="dropdown">
+        <div className="dropdown-btn" onClick={(e) => setIsActive(!isActive)}>
+          Sort by <AiFillCaretDown style={{ color: "white" }} />
         </div>
-        
-      <div className="container">
+        {isActive && (
+          <div className="dropdown-content">
+            <div
+              className="dropdown-item"
+              name="EXCELLENT"
+              onClick={handleClick}
+            >
+              Excellent
+            </div>
+            <div className="dropdown-item" name="GOOD" onClick={handleClick}>
+              Good
+            </div>
+            <div
+              className="dropdown-item"
+              name="SUFFICIENT"
+              onClick={handleClick}
+            >
+              Sufficient
+            </div>
+            <div className="dropdown-item" name="POOR" onClick={handleClick}>
+              Poor
+            </div>
+          </div>
+        )}
+      </div>
+
+      <div className="card-container">
         {data &&
           data.map((beach, key) => {
             return (
@@ -57,12 +77,16 @@ const Cards = () => {
                   <figcaption className="mainFigcap">
                     <hgroup className="hGroups">
                       <h3>
-                        {beach.name}, <br /> {beach.address}
+                        {beach.name}
+                        {/* , <br /> {beach.address} */}
                       </h3>
-                      <h4>Status: {beach.status}</h4>
+                      {/* <h4>Status: {beach.status}</h4> */}
                     </hgroup>
                     <p>{beach.description}</p>
                   </figcaption>
+                  <div className="arrow">
+                    <faCircleArrowRight />
+                  </div>
                 </figure>
               </NavLink>
             );
