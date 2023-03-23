5e04ef9b0df80f58b3f8df499d50b74f01180d8c
Erla-B <111748576+Erla-B@users.noreply.github.com>
2023-03-23 17:19:34 +0000

    Working on sign in

--- frontend/src/components/Pages/SignIn/SignIn.jsx
+++ frontend/src/components/Pages/SignIn/SignIn.jsx
@@ -1,89 +1,98 @@
-import './SignIn.scss'
-import { useState, useEffect } from 'react';
-import Cleaning from '../../../images/Rectangle 37.png';
-import userService from '../../../services/userService';
+import "./SignIn.scss";
+import { useState, useEffect } from "react";
+import userService from "../../../services/userService";
 
 const SignInPage = () => {
-    const initialValues = { email: '', password: ''};
-    const [formValues, setFormValues] = useState(initialValues);
-    const [formErrors, setFormErrors] = useState({});
-    const [isSubmit, setIsSubmit] = useState(false);
+  const initialValues = { email: "", password: "" };
+  const [formValues, setFormValues] = useState(initialValues);
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormValues({ ...formValues, [name]: value });   
-        console.log(e.target) 
-    };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+    console.log(e.target);
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        setFormErrors(validate(formValues));
-        setIsSubmit(true);
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormErrors(validate(formValues));
+    setIsSubmit(true);
+  };
 
-    useEffect(() => {
-        if (Object.keys(formErrors).length === 0 && isSubmit) {
-            userService.logIn(formValues.email, formValues.password).then(data => {
-                console.log("G")
-                localStorage.setItem("user", JSON.stringify(data.data))
-                window.location.href = "/"
-            })
-        }
-    }, [formErrors]);
+  useEffect(() => {
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      userService.logIn(formValues.email, formValues.password).then((data) => {
+        console.log("G");
+        localStorage.setItem("user", JSON.stringify(data.data));
+        window.location.href = "/";
+      });
+    }
+  }, [formErrors]);
 
-    const validate = (values) => {
-        const errors = {};
-        const regex =
-        /^[a-zA-Z0-9_-]{3,16}$/;
-        if (!values.email) {
-            errors.email = 'Username is required!';
-            
-        }
-        if (!values.password) {
-            errors.password = 'Password is required!';
-        } else if (values.password.length < 4) {
-            errors.password = "Password must be more than 4 characters";
-          } else if (values.password.length > 10) {
-            errors.password = "Password cannot exceed more than 10 characters";
-          }
-        return errors;
+  const validate = (values) => {
+    const errors = {};
+    const regex = /^[a-zA-Z0-9_-]{3,16}$/;
+    if (!values.email) {
+      errors.email = "Username is required!";
+    }
+    if (!values.password) {
+      errors.password = "Password is required!";
+    } else if (values.password.length < 4) {
+      errors.password = "Password must be more than 4 characters";
+    } else if (values.password.length > 10) {
+      errors.password = "Password cannot exceed more than 10 characters";
+    }
+    return errors;
+  };
 
-    };
-
-    return(
-
-<form className='form1' onSubmit={handleSubmit}>
+  return (
+    <div className="background" >
+      <form className="form1" onSubmit={handleSubmit}>
         {Object.keys(formErrors).length === 0 && isSubmit ? (
-            <div style={{color: 'green', fontSize: '40px'}} className='ui message success'>Signed in successfully</div>
-    ) : null}
-    <div className='containerss'>
-        {/* SIGN IN */}
-        <div className='signin'>
-        <h1 className='h1ss'>Sign In</h1>
-        <label htmlFor="email">Email</label>
-        <input name="email" type="email" value={formValues.email}
-        onChange={handleChange}/>
-        <p style={{color: 'red'}}>{formErrors.email}</p>
+          <div
+            style={{ color: "green", fontSize: "40px" }}
+            className="ui message success"
+          >
+            Signed in successfully
+          </div>
+        ) : null}
+        <div className="containerss">
+          {/* SIGN IN */}
+          <div className="signin">
+            <h1 className="h1ss">Welcome Back</h1>
+            <label htmlFor="email">Email/Username</label>
+            <input
+              name="email"
+              type="email"
+              placeholder="Email/Username"
+              value={formValues.email}
+              onChange={handleChange}
+            />
+            <p style={{ color: "red" }}>{formErrors.email}</p>
 
-        <label htmlFor="password">Password</label>
-        <input name="password" type="password" value={formValues.password}
-        onChange={handleChange}/>
-        <p style={{color: 'red'}}>{formErrors.password}</p>
-        <div className='notAMember'>Don't have an account? <a href='/register'>Register</a>
+            <label htmlFor="password">Password</label>
+            <input
+              name="password"
+              type="password"
+              placeholder="Password"
+              value={formValues.password}
+              onChange={handleChange}
+            />
+            <p style={{ color: "red" }}>{formErrors.password}</p>
+            <div className="notAMember">
+              Don't have an account? <a href="/register">Register</a>
+            </div>
+
+            <button>Log in</button>
+          </div>
         </div>
-
-        <button>Log in</button>
-        </div>
-        <img className='cleaning' src={Cleaning} alt="Beach Cleaning" />
-
+      </form>
     </div>
-</form>
+  );
+};
 
-        )
-}
-
-
-export default SignInPage
+export default SignInPage; /*}
 
 /*
 import './SignIn.scss'
@@ -93,8 +102,8 @@ const SignInPage = () => {
 
 <form className='form'>
     <div className='containerss'>
-        {/* SIGN IN *//*}
-       /* <div className='signin'>
+        {/* SIGN IN */ /*}
+/* <div className='signin'>
         <h1 className='h1ss'>Sign In</h1>
         <label htmlFor="username">Username</label>
         <input name="username" type="text" />
@@ -104,7 +113,7 @@ const SignInPage = () => {
         <button>Log in</button>
         </div>
 
-        {/* REGISTER *//*}
+        {/* REGISTER */
        /* <div className='register'>
         <h1 className='h1ss'>Sign Up</h1>
         <label htmlFor="fullname">Full name</label>
